498030b9f96234c2153330f22440ef760d388fd2
irfanYLDRM <yildirim.irfan@outlook.com>
2021-05-05 23:28:22 +0300

    inheritance örnek

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		
+	}
+
+}
