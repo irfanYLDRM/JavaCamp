473cfd678ec8ff3021806c4b8369c58874c02a7e
irfanYLDRM <yildirim.irfan@outlook.com>
2021-05-05 23:08:48 +0300

    intro

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,69 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String internetSubeButonu = "Ýnternet Þubesi";
+		double dolarDun = 8.20;
+		double dolarBugun = 8.20;
+		int vade = 36;
+		boolean dustuMU = false;
+		
+		System.out.println(internetSubeButonu);
+		
+		if(dolarBugun<dolarDun) {
+			System.out.println("Dolar Düþtü Resmi");
+		}else if(dolarBugun>dolarDun){
+			System.out.println("Dolar Yükseldi Resmi");
+		}else {
+			System.out.println("Dolar Eþittir Resmi");
+		}
+		
+		
+		String kredi1 = "Hýzlý Kredi";
+		String kredi2 = "Mutlu Emekli Kredisi";
+		String kredi3 = "Konut Kredisi";
+		String kredi4 = "Taþýt Kredisi";
+		String kredi5 = "Ýhtiyaç Kredisi";
+		
+		String[] krediler = {
+				"Hýzlý Kredi",
+				"Mutlu Emekli Kredisi",
+				"Konut Kredisi",
+				"Taþýt Kredisi",
+				"Ýhtiyaç Kredisi",
+				"Kamu Çalýþanlarýna Özel Kredi"
+			};
+		
+		//foreach
+		for(String kredi : krediler) {
+			System.out.println(kredi);
+		}
+		
+		for(int i=0; i<krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+		
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi1 = sayi2;
+		sayi2 = 100;
+		System.out.println(sayi1);
+		
+		int[] sayilar1 = {1,2,3,4,5};
+		int[] sayilar2 = {10,20,30,40,50};
+		sayilar1=sayilar2;
+		sayilar2[0]=100;
+		System.out.println(sayilar1[0]);
+		
+		String sehir1 = "Ankara";
+		String sehir2 = "Ýstanbul";
+		sehir1=sehir2;
+		sehir2 = "Ýzmir";
+		System.out.println(sehir1);
+		
+		
+	}
+
+}
